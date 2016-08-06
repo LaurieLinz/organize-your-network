6e2391e0e4b67fa5af0635f895adcc156075cb60
Laurie Linz <lvlinz@comcast.net>
2016-08-06 16:33:59 -0600

    Minor DB changes.

--- db/migrate/20160805000209_create_contacts.rb
+++ db/migrate/20160805000209_create_contacts.rb
@@ -1,14 +1,14 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.string :email
       t.string :phone
       t.string :met_on_social_platform
-      t.date :date_met
+      t.date :date_met, null: false
       t.string :availability
-      t.integer :skillset
+      t.integer :skillset, null: false
       t.text :notes
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -17,17 +17,17 @@ ActiveRecord::Schema.define(version: 20160805000209) do
   enable_extension "plpgsql"
 
   create_table "contacts", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "first_name",             null: false
+    t.string   "last_name",              null: false
     t.string   "email"
     t.string   "phone"
     t.string   "met_on_social_platform"
-    t.date     "date_met"
+    t.date     "date_met",               null: false
     t.string   "availability"
-    t.integer  "skillset"
+    t.integer  "skillset",               null: false
     t.text     "notes"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
